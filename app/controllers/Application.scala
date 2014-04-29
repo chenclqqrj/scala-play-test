ab2e8d2e9ebd61dd41871bdb9f7e0fff5165f297
chenclqqrj <chenclqqrj@gmail.com>
2014-04-29 18:05:37 +0800

    mm

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,9 +7,10 @@ import models.Person
 object Application extends Controller {
   var ps = List[Person](new Person(1, "张三", 35), new Person(2, "李四", 40))
 
-  def index = Action{
-    ps.+:(new Person(3, "旺旺", 55))
-    Ok(html.index(ps))
+  def index = Action {
+    //    ps.+:(new Person(3, "旺旺", 55))
+    //    Ok(html.index(ps))
+    Ok(html.test(ps))
   }
 
 }
---
+++ app/models/SayHello.java
@@ -0,0 +1,11 @@
+package models;
+
+/**
+ * Created by crjun on 14-4-29.
+ */
+public class SayHello {
+    public static String say() {
+        return "<h1>I am saying!!!</h1>";
+    }
+
+}
