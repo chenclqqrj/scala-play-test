7169da467cf4a502a86fce28d2ec7660866d60dd
chenclqqrj <chenclqqrj@gmail.com>
2014-05-04 13:23:58 +0800

    临时增加

--- app/models/MyHelper.scala
+++ app/models/MyHelper.scala
@@ -9,45 +9,51 @@ import scala.collection.immutable.ListMap
  */
 object MyHelper {
   // 获取到View的完整类路径 如 models.View
-  val viewPath = universe.typeOf[View].typeSymbol.owner.name + ".View"
-  //
+//  val viewPath = universe.typeOf[View].typeSymbol.owner.name + ".View"
+  val viewType = universe.typeOf[View]
+
+  def jarg(jargs, name: String) = jargs(newTermName(name)) match {
+    // Constant is always wrapped in a Literal or LiteralArgument tree node
+    case LiteralArgument(ct: Constant) => value
+    case _ => sys.error("Not a constant")
+  }
+
   def generateDataGridHeader[tpe:TypeTag] = {
     val ty:Type = universe.typeOf[tpe]
     val ty_as:List[universe.Annotation] = ty.typeSymbol.annotations
-    val view_anno:Option[Annotation] = ty_as.find(_.toString.startsWith(viewPath))
-    view_anno match {
-      case Some(content) => {
-        val args:ListMap[Name, JavaArgument] = content.javaArgs
-        var flag = false;
-        args.foreach(arg => {
-          arg._1.toString() match {
-            case "editable" => {
-              if (arg._2.toString.equalsIgnoreCase("true")) {
-                flag = true;
+    ty_as.foreach(ty_a => {
+      ty_a match {
+        case viewType =>
+          val args:ListMap[Name, JavaArgument] = ty_a.javaArgs
+          var flag = false;
+          args.foreach(arg => {
+            arg._1.toString() match {
+              case "editable" => {
+                if (arg._2.toString.equalsIgnoreCase("true")) {
+                  flag = true;
+                  println(arg._1.toString + "----" + arg._2.toString)
+                }
+              }
+              case "hasAddOperate" => {
+                //              if (flag) {
                 println(arg._1.toString + "----" + arg._2.toString)
+                //              }
+              }
+              case "hasRemoveOperate" => {
+                if (flag) {
+                  println(arg._1.toString + "----" + arg._2.toString)
+                }
+              }
+              case "hasUpdateOperate" => {
+                if (flag) {
+                  println(arg._1.toString + "----" + arg._2.toString)
+                }
               }
             }
-            case "hasAddOperate" => {
-//              if (flag) {
-                println(arg._1.toString + "----" + arg._2.toString)
-//              }
-            }
-            case "hasRemoveOperate" => {
-              if (flag) {
-                println(arg._1.toString + "----" + arg._2.toString)
-              }
-            }
-            case "hasUpdateOperate" => {
-              if (flag) {
-                println(arg._1.toString + "----" + arg._2.toString)
-              }
-            }
-          }
-        })
+          })
+        case _ => println("#############" + ty_a.tpe)
       }
-      case None =>
-    }
-    println("---" + view_anno.getOrElse(""))
+    })
   }
 
   def main(args:Array[String]) {
